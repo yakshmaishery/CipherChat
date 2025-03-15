9b816ef792e536d138b78f780ec7b1d6b2c1e307
yakshmaishery <yakshmaishery@users.noreply.github.com>
2025-03-15 18:40:00 +0530

    Removed File Upload menu

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -3,7 +3,7 @@
    export let Window = ""
    export let IsConnected = false
    import House from "@lucide/svelte/icons/house";
-   import { MessageCircle,Video,ScreenShare,Upload,Contact,Landmark } from "@lucide/svelte";
+   import { MessageCircle,Video,ScreenShare,Contact,Landmark } from "@lucide/svelte";
    import * as Sidebar from "$lib/components/ui/sidebar/index.js";3
    import DarkModeButton from '$lib/components/DarkModeButton.svelte';
    import '$lib/Styles/AppSliderCSS.css'
@@ -29,10 +29,6 @@
      icon: ScreenShare,
     },
     {
-     title: "File Share",
-     icon: Upload,
-    },
-    {
      title: "Tutorials",
      icon: Landmark,
     },
@@ -49,6 +45,7 @@
      <Sidebar.GroupLabel class="SliderHeader">
         {$AppName} <DarkModeButton/>
    </Sidebar.GroupLabel>
+   <br/>
      <Sidebar.GroupContent>
       <Sidebar.Menu>
        {#each items as item (item.title)}
