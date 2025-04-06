554e481add7111e2ebcd04678bd2ff5b7dc129aa
yakshmaishery <yakshmaishery@users.noreply.github.com>
2025-04-06 19:32:10 +0530

    another try for ICE

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,14 +42,18 @@
    let Progressmax = 0;
    let downloadfileList:{filename:string,base64:string,filesize:string,datetime:string}[] = []
    const shortdummyID = nanoid(4).toLowerCase() // Generate Random User ID
-   var peer = new Peer(shortdummyID,{debug:3,secure:true,config:{iceServers:[
-            {
-               "urls": "turn:relay.metered.ca:443",
-               "username": "efNzdT63A5L0vG0r6wh7G",
-               "credential": "hjaDGtqQvJYfKf3k"
-            }
-         ]
-      }}) // Create Peer
+   var peer = new Peer(shortdummyID,{debug:3,secure:true,config: {
+      iceServers: [
+         {
+         urls: "stun:stun.l.google.com:19302"
+         },
+         {
+         urls: "turn:relay.metered.ca:443",
+         username: "efNzdT63A5L0vG0r6wh7G",
+         credential: "hjaDGtqQvJYfKf3k"
+         }
+      ]
+   }}) // Create Peer
 
    peer.on("open",(id) => { // Connect Peer if Success set the ID
       UserID = id
